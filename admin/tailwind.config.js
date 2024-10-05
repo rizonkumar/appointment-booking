80d6aa3664ef216e9e78a76e577695b1c2e56a4a
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2024-10-05 08:30:29 +0530

    Feat: Imported custom font and color

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+        heading: ["Montserrat", "sans-serif"],
+        body: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#5f6FFF",
+          dark: "#4F5FEF",
+        },
+      },
+    },
   },
   plugins: [],
 };
