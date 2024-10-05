600de27027246d4b9afbda630a813367fbf05137
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2024-10-05 08:14:24 +0530

    Feat: Imported Toastify, NavBar and Login

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,23 @@
+import Login from "./pages/Login";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useContext } from "react";
+import { AdminContext } from "./context/adminContext";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return (
+  const { aToken } = useContext(AdminContext);
+  return aToken ? (
     <>
-      <div className="">App</div>
+      <div className="bg-[#F8F9FD]">
+        <ToastContainer />
+        <NavBar />
+      </div>
+    </>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
     </>
   );
 }
